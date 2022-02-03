a6d2370403530d7cfdf465a4d84503de3f2d9451
Meenakshi Sundaram Koushik <meenakshi.koushik@dedalus.com>
2022-02-03 14:10:43 +0100

    import initial project

---
+++ src/main/java/org/springframework/amqp/tutorials/learnrabbitmq/LearnrabbitmqApplication.java
@@ -0,0 +1,13 @@
+package org.springframework.amqp.tutorials.learnrabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnrabbitmqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnrabbitmqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/springframework/amqp/tutorials/learnrabbitmq/LearnrabbitmqApplicationTests.java
@@ -0,0 +1,13 @@
+package org.springframework.amqp.tutorials.learnrabbitmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnrabbitmqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
